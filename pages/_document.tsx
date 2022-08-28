bb36c3cbebb5729bdcf7dc9135bc27e389fccab4
AustinWildgrube <austinwildgrube@gmail.com>
2022-08-28 12:32:24 -0500

    fix: use next js font optimization

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 import { createGetInitialProps } from '@mantine/next';
 
@@ -6,6 +6,24 @@ const getInitialProps = createGetInitialProps();
 
 class _Document extends Document {
   static getInitialProps = getInitialProps;
+
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default _Document;
